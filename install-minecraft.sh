81fc7839b985bdda7dcdcca808aaaa96d4cf70a0
Michael Karneim <mkarneim@googlemail.com>
2019-11-03 10:38:59 +0100

    Update install-minecraft.sh

--- install-minecraft.sh
+++ install-minecraft.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 sudo adduser --disabled-password --gecos "" minecraft
-
+sudo apt-get update
+sudo apt-get -y install openjdk-8-jre-headless
+sudo apt-get -y remove openjdk-11-jre-headless
+sudo -u minecraft wget -P /home/minecraft https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
+sudo su minecraft -c "echo 'eula=true' > /home/minecraft/eula.txt"
