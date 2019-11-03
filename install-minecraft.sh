33f0cdff11dddf03d72ab62cf5e4287ad7504dcf
Michael Karneim <mkarneim@googlemail.com>
2019-11-03 10:47:41 +0100

    Update install-minecraft.sh

--- install-minecraft.sh
+++ install-minecraft.sh
@@ -5,3 +5,4 @@ sudo apt-get -y install openjdk-8-jre-headless
 sudo apt-get -y remove openjdk-11-jre-headless
 sudo -u minecraft wget -P /home/minecraft https://launcher.mojang.com/v1/objects/3dc3d84a581f14691199cf6831b71ed1296a9fdf/server.jar
 sudo su minecraft -c "echo 'eula=true' > /home/minecraft/eula.txt"
+cp start.sh ~
